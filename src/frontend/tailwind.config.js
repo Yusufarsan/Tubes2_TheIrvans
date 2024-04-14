7598322482a241660b7342c4ee188adaffc61bdd
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-04-15 01:48:56 +0700

    Add Navbar

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -17,6 +17,10 @@ module.exports = {
       },
     },
     extend: {
+        fontFamily: {
+            Lobster: ['Lobster'],
+            Akaya: ['Akaya Kanadaka'],
+        },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",

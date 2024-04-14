f4e3a52e86fc3928e411815a3e11914969c8d44f
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-04-15 01:49:18 +0700

    image

--- src/frontend/src/routes/about.lazy.tsx
+++ src/frontend/src/routes/about.lazy.tsx
@@ -1,9 +1,15 @@
 import { createLazyFileRoute } from "@tanstack/react-router"
+import Navbar from "@/components/ui/navbar"
 
 export const Route = createLazyFileRoute("/about")({
   component: About,
 })
 
 function About() {
-  return <div className="p-2">Hello from About!</div>
+  return (
+    <>
+      <Navbar />
+      {/* <img src="/stima2-bg.jpg" alt="background Image"></img> */}
+    </>
+  )
 }

21dabc88c44b8a5fc6ea926a51f9cade40aadcf0
Yusufarsan <sandiyusufardian@gmail.com>
2024-04-19 17:37:03 +0700

    Added home-switch component

---
+++ src/frontend/src/components/ui/home-switch.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { Switch } from "@/components/ui/switch"
+import { useState } from "react"
+
+function HomeSwitch() {
+  const [checked, setChecked] = useState(false)
+  console.log(checked)
+
+  return (
+    <>
+      <div className="flex space-x-1 font-Akaya">
+        <Switch onClick={() => setChecked(!checked)} />
+        {checked ? <p>On</p> : <p>Off</p>}
+      </div>
+    </>
+  )
+}
+
+export default HomeSwitch

e73043812fb4fef33a63c33004fb5cf3789b63bf
SandWithCheese <naufalahmad022@gmail.com>
2024-04-19 18:07:20 +0700

    feat: initialize api endpoint for bfs

---
+++ src/backend/bfs.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"sync"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type SafeMap[T any] struct {
+	mu   sync.Mutex
+	data map[string]T
+}
+
+func (s *SafeMap[T]) Add(key string, value T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
+}
+
+func (s *SafeMap[T]) Get(key string) (T, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	value, ok := s.data[key]
+	return value, ok
+}
+
+type SafeArray[T any] struct {
+	mu    sync.Mutex
+	array []T
+}
+
+func (s *SafeArray[T]) Add(value T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.array = append(s.array, value)
+}
+
+func (s *SafeArray[T]) Get() []T {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	return s.array
+}
+
+func (s *SafeArray[T]) Set(array []T) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.array = array
+}
+
+func makeRequest(url string) *goquery.Document {
+	res, err := http.Get(url)
+
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+
+	if err != nil {
+		return nil
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+
+	if err != nil {
+		return nil
+	}
+
+	return doc
+}
+
+func bfs(startURL string, endURL string, baseURL string) [][]string {
+	visitedURL := SafeMap[bool]{data: make(map[string]bool)}
+	correctURL := SafeMap[[]string]{data: make(map[string][]string)}
+	queriedURL := SafeMap[bool]{data: make(map[string]bool)}
+	found := false
+
+	var paths SafeArray[[]string]
+	paths.Add([]string{startURL})
+
+	var solutions SafeArray[[]string]
+
+	for !found {
+		var newPaths SafeArray[[]string]
+		pathsSize := len(paths.Get())
+		var wg sync.WaitGroup
+		wg.Add(pathsSize)
+
+		maxConcurrentRequests := 250
+
+		sem := make(chan struct{}, maxConcurrentRequests)
+
+		for i := 0; i < pathsSize; i++ {
+			sem <- struct{}{}
+			go func(i int) {
+				defer func() { <-sem }()
+				defer wg.Done()
+				p := paths.Get()[i]
+				node := p[len(p)-1]
+
+				if value, ok := correctURL.Get(node); ok {
+					fmt.Println("Correct URL")
+					for _, path := range value {
+						newPath := append([]string{}, p...)
+						newPath = append(newPath, path)
+						newPaths.Add(newPath)
+					}
+					return
+				}
+
+				if _, ok := queriedURL.Get(node); ok {
+					fmt.Println("Already queried")
+					return
+				}
+
+				doc := makeRequest(node)
+
+				if doc != nil {
+					duplicateURL := make(map[string]bool)
+					doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+						link, _ := s.Attr("href")
+						matched, _ := regexp.MatchString("^/wiki/", link)
+
+						if matched && !duplicateURL[baseURL+link] {
+							duplicateURL[baseURL+link] = true
+
+							if baseURL+link == endURL {
+								fmt.Println("Found!")
+								value, _ := correctURL.Get(node)
+								correctURL.Add(node, append(value, baseURL+link))
+								found = true
+								solutions.Add(append(p, baseURL+link))
+							}
+
+							_, ok := visitedURL.Get(baseURL + link)
+							if !found && !ok {
+								visitedURL.Add(baseURL+link, true)
+								newPath := make([]string, len(p))
+								copy(newPath, p)
+								newPath = append(newPath, baseURL+link)
+								newPaths.Add(newPath)
+							}
+						}
+					})
+					queriedURL.Add(node, true)
+				}
+			}(i)
+		}
+
+		wg.Wait()
+		paths.Set(newPaths.Get())
+	}
+
+	return solutions.Get()
+}
---
+++ src/backend/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type linkJson struct {
+	Start string `json:"start"`
+	End   string `json:"end"`
+}
+
+var baseURL = "https://en.wikipedia.org"
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/multiple/bfs", func(c *gin.Context) {
+		var link linkJson
+
+		if err := c.BindJSON(&link); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		start := link.Start
+		end := link.End
+
+		result := bfs(start, end, baseURL)
+
+		c.JSON(http.StatusOK, result)
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}

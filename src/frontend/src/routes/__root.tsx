c7342905923c829288a093fad5880a605efdace3
Shafiq Irvansyah <68421940+shafiqIrv@users.noreply.github.com>
2024-04-19 20:25:59 +0700

    dix: navbar

--- src/frontend/src/routes/__root.tsx
+++ src/frontend/src/routes/__root.tsx
@@ -4,35 +4,34 @@ import { ThemeProvider } from "@/components/theme-provider"
 const Navbar = () => {
   return (
     <nav>
-      <div
-        className="flex  flex-row justify-between bg-primary font-Lobster text-foreground  shadow-md shadow-slate-500  transition-opacity duration-500 ease-in-out"
-        data-replace='{ "opacity-0": "opacity-100" }'
-      >
-        <Link to="/" className="ml-14 p-2">
-          <div className="flex flex-row transition hover:scale-90 active:scale-75">
+      <div className="flex h-20 items-center justify-between bg-primary px-14 font-Lobster text-foreground shadow-md shadow-slate-500">
+        <Link to="/">
+          <div className="flex items-center gap-4 transition hover:scale-90 active:scale-75">
             <img
-              className=" flex w-12  scale-90 items-center shadow-none"
+              className="flex w-12 scale-90 items-center shadow-none"
               src="/logo.png"
               alt="logo"
             />
-            <div className="ml-4 ">
-              <p>Tugas Besar 2 Stima</p>
-              <p className=" flex font-Akaya  ">By TheIrvans</p>
+            <div className="flex flex-col">
+              <p className="text-[28px] shadow-slate-500">
+                Tugas Besar 2 Stima
+              </p>
+              <p className="font-Akaya text-[15px]">By TheIrvans</p>
             </div>
           </div>
         </Link>
 
         <Link
           to="/about"
-          className="mr-14 flex transition hover:scale-90 active:scale-75"
+          className=" flex transition hover:scale-90 active:scale-75"
         >
-          <div className="mr-4  flex items-center">
+          <div className="flex items-center gap-4">
             <img
               src="/info.png"
               alt="info logo"
               className="scale-25 drop-shadow-2xl"
             />
-            <div className=" ml-2 shadow-2xl ">About</div>
+            <div className="text-[24px] shadow-2xl">About</div>
           </div>
         </Link>
       </div>

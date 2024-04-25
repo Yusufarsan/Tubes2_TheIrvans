22ccb62bd21da340301e8c0b651b79f0ac0d2696
SandWithCheese <naufalahmad022@gmail.com>
2024-04-25 10:32:09 +0700

    feat: finishing ids algorithm and connect it to backend

--- src/backend/ids.go
+++ src/backend/ids.go
@@ -165,3 +165,147 @@ func ids(startURL string, endURL string, baseURL string) ([][]string, int) {
 
 	return paths, visitedCount
 }
+
+func (n *Node) DFSSingle(baseURL string, endURL string) ([][]string, int) {
+	visited := make(map[string]bool) // Map untuk nyimpen node yang udah dikunjungi
+	paths := [][]string{}
+	// countArticle := 1
+
+	// n.mu.Lock()
+	// defer n.mu.Unlock()
+
+	found := false
+	n.dfsRecursiveSingle(baseURL, endURL, visited, []string{}, &paths, &found)
+	// fmt.Println(visited)
+	return paths, len(visited)
+}
+
+func (n *Node) dfsRecursiveSingle(baseURL string, endURL string, visited map[string]bool, currentPath []string, paths *[][]string, found *bool) {
+	// If node is nil, stop recursion
+	if n == nil {
+		return
+	}
+
+	visited[n.Value] = true
+	currentPath = append(currentPath, n.Value)
+
+	allVisited := true
+	for _, child := range n.Children {
+		if !visited[child.Value] {
+			allVisited = false
+			child.dfsRecursiveSingle(baseURL, endURL, visited, currentPath, paths, found)
+			if *found {
+				break // Stop recursing if a solution has been found
+			}
+		}
+	}
+
+	if allVisited && !*found {
+		if n.Value == endURL {
+			fmt.Println("Found endURL when traversing!")
+			pathCopy := make([]string, len(currentPath))
+			copy(pathCopy, currentPath)
+			*paths = append(*paths, pathCopy)
+			*found = true // Mark that a solution has been found
+		}
+	}
+
+	visited[n.Value] = false
+}
+
+func ids_single(startURL string, endURL string, baseURL string) ([][]string, int) {
+	visitedURL := SafeMap[bool]{data: make(map[string]bool)}
+	// Inisialisasi root sebagai startURL nya dan querriedURL sebagai map untuk nyimpen URL yang udah di query
+	queriedURL := SafeMap[bool]{data: make(map[string]bool)} // Agak ga penting soalnya hampir ga kepake
+	root := NewNode(startURL)
+
+	// fmt.Println("Depth-First Search:")
+	// Inisialisasi paths untuk nyimpen solusi dan visitedCount untuk ngitung berapa artikel yang diperiksa
+	var paths = make([][]string, 0)
+	var visitedCount int
+	// var countArticle int
+
+	// Inisialisasi currentLastNodes untuk nyimpen semua simpul pada satu layer tertentu (bergantung di depth mana sehingga nilainya akan berubah ubah)
+	var currentLastNodes SafeArray[*Node]
+	currentLastNodes.Add(root)
+
+	// Inisialisasi depth untuk nyimpen data kedalaman yang bakal ditelusuri, ini akan di increment trs sampe ketemu
+	depth := 0
+
+	// Inisialisasi found untuk nyimpen apakah udah ketemu solusi atau belum
+	found := false
+
+	// Selama belom ketemu satupun solusi, bakalan di loop
+	for len(paths) == 0 {
+		var newLastNodes SafeArray[*Node] // Inisialisasi newLastNodes untuk memperbarui currentLastNodes (newLastNodes adalah semua simpul setelah layer nya currentLastNodes)
+
+		var wg sync.WaitGroup
+		wg.Add(len(currentLastNodes.Get()))
+
+		maxConcurrentRequests := 250
+
+		sem := make(chan struct{}, maxConcurrentRequests)
+
+		for _, node := range currentLastNodes.array { // iterasi semua simpul pada layer tertentu (pada currentLastNodes)
+			sem <- struct{}{}
+			go func(node *Node) {
+				defer func() { <-sem }()
+				defer wg.Done()
+
+				if found {
+					return
+				}
+
+				if _, ok := queriedURL.Get(node.Value); ok { // Kalo udah pernah di query, ga perlu di query lagi (Tapi jarang masuk ke kondisi ini makanya rada ga penting)
+					fmt.Println("Already queried")
+					return
+				}
+
+				doc := makeRequest(node.Value) // Nge scrap ke Wikipedia
+				if doc != nil {
+					duplicateURL := make(map[string]bool)
+					doc.Find("a").Each(func(_ int, s *goquery.Selection) { // Menemukan semua link yang ada di suatu halaman Wikipedia
+						link, _ := s.Attr("href")
+						matched, _ := regexp.MatchString("^/wiki/", link)
+
+						if matched && !duplicateURL[baseURL+link] { // Kalo link nya adalah link ke artikel Wikipedia lain dan belum pernah di cek
+							duplicateURL[baseURL+link] = true
+							// fmt.Println(baseURL + link)
+
+							if endURL == baseURL+link && !found {
+								fmt.Println("Found endURL when querying!")
+								node.AddChild(baseURL + link)
+								newLastNodes.Add(node.Children[baseURL+link])
+								found = true
+							}
+
+							_, ok := visitedURL.Get(baseURL + link)
+							if !ok {
+								node.AddChild(baseURL + link)                 // nambahin semua anakan dari node
+								newLastNodes.Add(node.Children[baseURL+link]) // Membentuk layer baru
+								visitedURL.Add(baseURL+link, true)
+							}
+						}
+					})
+				}
+			}(node)
+		}
+
+		wg.Wait()
+
+		currentLastNodes.Set(newLastNodes.Get()) // Update currentLastNodes dengan newLastNodes, yaitu layer berikutnya
+
+		fmt.Println("Recursing")
+		paths, visitedCount = root.DFSSingle(baseURL, endURL)
+		fmt.Println("Finished Recursing")
+
+		depth++ // Increment depth
+		fmt.Println("Depth:", depth)
+	}
+
+	// fmt.Println(paths)
+	// fmt.Println(visitedCount)
+	// fmt.Println(countArticle)
+
+	return paths, visitedCount
+}
--- src/backend/main.go
+++ src/backend/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"time"
 
@@ -66,35 +65,71 @@ func setupRouter() *gin.Engine {
 		c.JSON(http.StatusOK, gin.H{"result": result, "articles_count": visitedURLCount, "time_elapsed": time_elapsed.Milliseconds()})
 	})
 
+	r.POST("/multiple/ids", func(c *gin.Context) {
+		var link linkJson
+
+		if err := c.BindJSON(&link); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		start := link.Start
+		end := link.End
+
+		time_start := time.Now()
+		result, visitedURLCount := ids(start, end, baseURL)
+		time_elapsed := time.Since(time_start)
+
+		c.JSON(http.StatusOK, gin.H{"result": result, "articles_count": visitedURLCount, "time_elapsed": time_elapsed.Milliseconds()})
+	})
+
+	r.POST("/single/ids", func(c *gin.Context) {
+		var link linkJson
+
+		if err := c.BindJSON(&link); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		start := link.Start
+		end := link.End
+
+		time_start := time.Now()
+		result, visitedURLCount := ids_single(start, end, baseURL)
+		time_elapsed := time.Since(time_start)
+
+		c.JSON(http.StatusOK, gin.H{"result": result, "articles_count": visitedURLCount, "time_elapsed": time_elapsed.Milliseconds()})
+	})
+
 	return r
 }
 
-// func main() {
-// 	r := setupRouter()
-// 	// Listen and Server in 0.0.0.0:8080
-// 	r.Run(":8080")
-// }
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
 
 // Ini main untuk nge test IDS
-func main() {
-	startURL := "https://en.wikipedia.org/wiki/Joko_Widodo"
-	endURL := "https://en.wikipedia.org/wiki/Philosophy"
+// func main() {
+// 	startURL := "https://en.wikipedia.org/wiki/Joko_Widodo"
+// 	endURL := "https://en.wikipedia.org/wiki/Philosophy"
 
-	time_start := time.Now()
+// 	time_start := time.Now()
 
-	paths, visitedCount := ids(startURL, endURL, baseURL)
-	time_elapsed := time.Since(time_start)
+// 	paths, visitedCount := ids(startURL, endURL, baseURL)
+// 	time_elapsed := time.Since(time_start)
 
-	// Menghitung artikel yang dilalui
-	// var countArticle int
-	// for _, path := range paths {
-	// 	countArticle += len(path)
-	// }
-	// countArticle -= 2*(len(paths)-1)
+// 	// Menghitung artikel yang dilalui
+// 	// var countArticle int
+// 	// for _, path := range paths {
+// 	// 	countArticle += len(path)
+// 	// }
+// 	// countArticle -= 2*(len(paths)-1)
 
-	fmt.Println("Depth-First Search:")
-	fmt.Println("Result:", paths)
-	fmt.Println("Checked articles:", visitedCount) // Artikel yang diperiksa
-	// fmt.Println("Visited article inside the paths:", countArticle)		// Artikel yang dilalui (menuju ke endURL) ----- jadinya diitung di FE aja
-	fmt.Println("Time Elapsed:", time_elapsed)
-}
+// 	fmt.Println("Depth-First Search:")
+// 	fmt.Println("Result:", paths)
+// 	fmt.Println("Checked articles:", visitedCount) // Artikel yang diperiksa
+// 	// fmt.Println("Visited article inside the paths:", countArticle)		// Artikel yang dilalui (menuju ke endURL) ----- jadinya diitung di FE aja
+// 	fmt.Println("Time Elapsed:", time_elapsed)
+// }
